e1785328869630b5695544e8cd59fd7cde815e1b
deanerphelps <deanerphelps@gmail.com>
2022-08-05 18:02:27 -0400

    Cleaned up. Created Player Script and Movement.

--- WarpSpeed/Assets/Scripts/Player/Player.cs
+++ WarpSpeed/Assets/Scripts/Player/Player.cs
@@ -4,15 +4,38 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Rigidbody2D body;
+    public float speed = 1f;
+       
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();        
+        speed = 5;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Movement();
     }
+
+    void Movement()
+    {
+        Vector3 direction = new Vector3(Input.GetAxisRaw("Horizontal"),
+        Input.GetAxisRaw("Vertical"), 0);
+
+        transform.position = Vector2.MoveTowards(transform.position, transform.position
+        + direction, speed * Time.deltaTime);
+
+        if (Input.GetKey("left shift"))
+        {
+            speed = 7;
+        }
+        else
+        {
+            speed = 5;
+        }
+    }
+
 }
